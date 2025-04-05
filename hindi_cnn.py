f2f1f97d07c21851c3671ad7d4527cec707eb376
Rhushil Vasavada <126303607+rvasav26@users.noreply.github.com>
2025-04-05 13:44:17 -0500

    Update hindi_cnn.py

--- hindi_cnn.py
+++ hindi_cnn.py
@@ -1,11 +1,11 @@
-#Author: Rhushil Vasavada
-#Devanagari Character CNN TensorFlow Model
-#Description: This is a custom-built convolutional neural network (CNN) that 
-#has been trained on 9,000+ handwritten Devanagari characters. It contains several
-#layers such as max pooling and flattening to perform matrix transformations 
-#on the input matrix representing a 2D array of the handwritten character (in grayscale).
+# Author: Rhushil Vasavada
+# Devanagari Character CNN TensorFlow Model
+# Description: This is a custom-built convolutional neural network (CNN) that 
+# has been trained on 9,000+ handwritten Devanagari characters. It contains several
+# layers built with Keras such as max pooling and flattening to perform matrix transformations 
+# on the input matrix representing a 2D array of the handwritten character (in grayscale).
 
-#import libraries
+# import libraries
 import tensorflow as tf
 import pickle
 from keras.preprocessing.image import ImageDataGenerator
@@ -13,7 +13,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from keras.callbacks import EarlyStopping
 
-#create a generator to be used to manipulate training data
+# create a generator to be used to manipulate training data
 trainDataGen = ImageDataGenerator(
     rotation_range=5,
     width_shift_range=0.1,
@@ -24,25 +24,18 @@ trainDataGen = ImageDataGenerator(
     horizontal_flip=False,
     fill_mode='nearest')
 
-#create a generator to be used to manipulate testing data
+# create a generator to be used to manipulate testing data
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-#load in training data 
+# load in training data 
 train_generator = trainDataGen.flow_from_directory(
     "DevanagariHandwrittenCharacterDataset/Train",
     target_size=(32, 32),
     batch_size=32,
     color_mode="grayscale",
     class_mode="categorical")
-prev = ""
 
-#these are the hindi characters that make up the training data (in english forms)
-labels = ["ka", "kha", "ga", "gha", "kna", "cha", "chha", "ja", "jha", "yna", "t`a", "t`ha", "d`a", "d`ha", "adna",
-          "ta", "tha", "da", "dha", "na", "pa", "pha", "ba", "bha", "ma", "yaw", "ra", "la", "waw", "sha", "shat", "sa",
-          "ha", "aksha", "tra", "gya", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-count = 0;
-
-#load in testing data
+# load in testing data
 validation_generator = test_datagen.flow_from_directory(
     "DevanagariHandwrittenCharacterDataset/Test",
     target_size=(32, 32),
@@ -50,10 +43,10 @@ validation_generator = test_datagen.flow_from_directory(
     color_mode="grayscale",
     class_mode='categorical')
 
-#build model (with TensorFLow sequential framework to add several layers) 
+# build model (with TensorFLow sequential framework to add several layers) 
 model = Sequential()
 
-#add convolutional layers
+# add convolutional layers
 model.add(Conv2D(filters=32,
                  kernel_size=(3, 3),
                  strides=1,
@@ -68,12 +61,12 @@ model.add(Conv2D(filters=32,
                  activation="relu",
                  input_shape=(32, 32, 1)))
 
-#add max pooling layers
+# add max pooling layers
 model.add(MaxPool2D(pool_size=(2, 2),
                     strides=(2, 2),
                     padding="same"))
 
-#more convolutional layers
+# more convolutional layers
 model.add(Conv2D(filters=64,
                  kernel_size=(3, 3),
                  padding='same',
@@ -86,12 +79,12 @@ model.add(Conv2D(filters=64,
                  padding='same',
                  activation="relu"))
 
-#max pooling
+# max pooling
 model.add(MaxPool2D(pool_size=(2, 2),
                     strides=(2, 2),
                     padding="same"))
 
-#more layers
+# more layers
 model.add(Dropout(0.2))
 
 model.add(Flatten())
@@ -108,8 +101,6 @@ model.add(Dense(46,
                 activation="softmax",
                 kernel_initializer="uniform"))
 
-#finally, we are using adam optmizer for gradient descent 
-#and other parameters to compile model
 model.compile(optimizer="adam",
               loss="categorical_crossentropy",
               metrics=["accuracy"])
@@ -118,12 +109,12 @@ print(model.summary())
 
 early_stopping_monitor = EarlyStopping(patience=2)
 
-#this essentially fits the model on the training data, with 10 epochs and specifies other parameters to enhance model performance
+# this essentially fits the model on the training data, with 10 epochs and specifies other parameters to enhance model performance
 with tf.device('/gpu:0'):
     history = model.fit_generator(train_generator, epochs=10, validation_data=validation_generator,
                                   steps_per_epoch=2444, validation_steps=432, use_multiprocessing=True,
                                   callbacks=[early_stopping_monitor])
 
-#save model as pickle file to be used in scratchpad application
-model.save("hindi_OCR_cnn_model_tf2.h5")
+# save model as pickle file to be used in other applications
+model.save("hindi_cnn_weights_tf2.h5")
 pickle.dump(model, open('hindi_OCR_cnn_model_pickle.pkl2', 'wb'))
